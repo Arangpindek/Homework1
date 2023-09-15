570b170ce54f2f911d71bc52cf871401b208504f
Max Nikolaevichev <Nikolaevichevm2@mail.ru>
2023-09-16 02:18:59 +0500

    completed hw3

--- script.js
+++ script.js
@@ -52,3 +52,65 @@ alert(user[info]);
 //Задание 10
 let z10 = prompt("Как вас зовут");
 alert(`Привет, ${z10} !`)
+
+
+// 3 домашка
+// Задание 1
+let password = 'пароль';
+let passchek = prompt('Введите пароль');
+if (password == passchek) {
+    alert("Пароль введен верно")
+}
+else {
+    alert("Пароль введен неверно")
+}
+// Задание 2
+let c = Number(prompt('Введите число'));
+if (c > 0 && c < 10) {
+    console.log('true')
+} else {
+    console.log('false')
+}
+// Задание 3
+let d = Number(prompt('Введите число 1'));
+let e = Number(prompt('Введите число 2'));
+if (d > 100 || e > 100) {
+    console.log('true')
+} else {
+    console.log('false')
+}
+// Задание 4
+let a = '2';
+let b = '3';
+// Код выше изменять менять нельзя, чтобы решить задачу исправьте код ниже: 
+alert(+a + +b);
+// Задание 5
+let month = Number(prompt('Введите номер месяца'))
+switch (month) {
+    case 1 || 2 || 12:
+        console.log('Это зима');
+        break;
+    case 3 || 4 || 5:
+        console.log('Это весна');
+        break;
+    case 6 || 7 || 8:
+        console.log('Это лето');
+        break;
+    case 9 || 10 || 11:
+        console.log('Это зима');
+        break;
+    default: console.log('Ты на какой планете живешь?');
+        break;
+}
+
+// Задание 7
+let chet = prompt('Введите число')
+let chetzna = chet % 2
+if (isNaN(chetzna)) {
+    alert('Введено не число')
+} else
+    if (chetzna == 0) {
+        alert('Число четное')
+    } else {
+        alert('Число не четное')
+    }
