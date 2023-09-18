9058bada5fb3593a04ebf800672eb996b4115c6f
Max Nikolaevichev <Nikolaevichevm2@mail.ru>
2023-09-18 22:46:50 +0500

    hw 4 completed

---
+++ script2.js
@@ -0,0 +1,60 @@
+// Задание 1
+let i1 = 1;
+do {
+    console.log('Привет');
+    i1++;
+}
+while (i1 <= 2);
+// 
+let i1 = 1;
+while (i1 <= 2) {
+    console.log('Привет');
+    i1++;
+}
+
+// Задание 2
+
+let i2 = 1;
+while (i2 <= 5) {
+    console.log(i2);
+    i2++;
+}
+
+// Задание 3
+
+let i3 = 7;
+while (i3 <= 22) {
+    console.log(i3);
+    i3++;
+}
+
+// Задание 4
+
+const salary = {
+    'Коля': '200',
+    'Вася': '300',
+    'Петя': '400'
+};
+for (let payment in salary) {
+    console.log(`${payment} - зарплата ${salary[payment]}$`);
+}
+// Задание 5
+
+let n = 1000;
+let num = 0;
+do {
+    n /= 2;
+    num++;
+}
+while (n >= 50)
+console.log(num);
+// Задание 6
+let e = Number(prompt('Введите число первой пятницы'))
+if (e > 7) {
+    alert('Вы ввели не то число')
+} else {
+    do {
+        e += 7;
+        alert(`Сегодня пятница, ${e}-е число. Необходимо подготовить отчет.`)
+    } while (e < 31);
+}
