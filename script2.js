3e942f26b2e88f2054936a76f3f3ff0448e43acc
Max Nikolaevichev <Nikolaevichevm2@mail.ru>
2023-09-22 01:56:46 +0500

    part of hw done

--- script2.js
+++ script2.js
@@ -53,3 +53,81 @@ if (e > 7) {
         alert(`Сегодня пятница, ${e}-е число. Необходимо подготовить отчет.`)
     } while (e < 31);
 }
+
+// Домашка 5
+//Задание 1
+function smallerNum(a, b) {
+    if (a > b) {
+        return b;
+    } else {
+        return a;
+    }
+}
+console.log(smallerNum(2, 4));
+console.log(smallerNum(6, 6));
+console.log(smallerNum(8, 4));
+//Задание 2
+let num = prompt("Введите число?");
+
+if (num % 2 == 0) {
+
+    function printMessage() {
+        console.log("Число чётное!");
+    }
+
+} else {
+
+    function printMessage() {
+        console.log("Число нечётное!");
+    }
+
+}
+
+printMessage();
+//Задание 3.1
+
+let a = prompt('введите число')
+
+function getSquare(num) {
+    console.log(num * num);
+}
+
+getSquare(a);
+
+//Задание 3.2
+let a = prompt('введите число')
+
+function getSquare(num) {
+    return num * num
+}
+
+console.log(getSquare(a));
+
+// Задание 4
+let age = prompt('введите ваш возраст')
+function ageRequest(num) {
+    if (num < 0) {
+        console.log('Вы ввели неправильное значение');
+    } else {
+        if (num >= -1 && num < 13) {
+            console.log('Привет, друг!');
+        } else {
+            console.log('Добро пожаловать!');
+        }
+    }
+}
+ageRequest(age);
+
+//Задание 5
+let Num1 = prompt('введите 1 число')
+let Num2 = prompt('введите 2 число')
+
+function numberCheker(n1, n2) {
+    if (isNaN(n1) || isNaN(n2)) {
+        console.log('Одно или оба значения не являются числом');
+    } else {
+        console.log(n1 * n2);
+    }
+}
+
+numberCheker(Num1, Num2);
